1c84cbd4a54dfa61b5d3d83e358e0ff6d1fb396f
TakarinTD <56709998+TakarinTD@users.noreply.github.com>
2020-11-28 14:56:09 +0700

    database

--- text normalization.sql
+++ text normalization.sql
@@ -1,19 +1,19 @@
 CREATE SCHEMA `text_normalization` ;
 
 CREATE TABLE `text_normalization`.`xep_hang` (
-  `id` INT NOT NULL,
+  `id_rank` INT NOT NULL,
   `van_ban_truoc_chuan_hoa` NVARCHAR(2048) NULL,
   `van_ban_sau_chuan_hoa` NVARCHAR(2048) NULL,
   `phat_am` VARCHAR(2048) NULL,
   `danh_gia` INT NULL,
-  PRIMARY KEY (`id`));
+  PRIMARY KEY (`id_rank`));
   
   CREATE TABLE `text_normalization`.`du_lieu_chuan` (
-  `id` INT NOT NULL,
+  `id_data` INT NOT NULL,
   `van_ban_can_chuan_hoa` NVARCHAR(2048) NULL,
   `van_ban_duoc_chuan_hoa` NVARCHAR(2048) NULL,
-  PRIMARY KEY (`id`),
-  CONSTRAINT `id`
-    FOREIGN KEY (`id`)
-    REFERENCES `text_normalization`.`xep_hang` (`id`)
-    );
+  `phat_am` VARCHAR(2048) NULL,
+  `rank_id` int(11)  NULL,
+  PRIMARY KEY (`id_data`),
+  FOREIGN KEY (`rank_id`) REFERENCES `text_normalization`.`xep_hang` (`id_rank`)
+);
